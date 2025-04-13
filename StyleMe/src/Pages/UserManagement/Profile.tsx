62912d5d243a4f6e8243cde31422f92606aba375
transebastian <transebastian21@gmail.com>
2025-04-12 21:08:01 -0400

    Profile features and pose detection package requirements

--- StyleMe/src/Pages/UserManagement/Profile.tsx
+++ StyleMe/src/Pages/UserManagement/Profile.tsx
@@ -1,19 +1,51 @@
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-
+const header = import.meta.env.VITE_API_URL
 
 const Profile = ({setJwt}:{setJwt: (jwt: null) => void}) => {
+   
     const navigate = useNavigate();
+    const [outfits, setOutfits] = useState<any[]>([]);
 
     const onLogOut = () => {
         setJwt(null)
         navigate("/");
     }
-    
+
+    useEffect(() => {
+        const headers = { 'Authorization': 'Bearer ' + localStorage.getItem('jwtToken')};
+        fetch(header + "/UserData/useroutfits", { headers })
+        .then(res =>  res.json())    
+        .then (data => setOutfits(data));
+    }, [])
+ 
     return (
         <>
             Work in progress!
             <a onClick={() => onLogOut()}>sign out!</a>
+
+            {outfits.map(outfit => (
+                <div key={outfit.id}>
+                    {outfit.id}
+                    {outfit.items.map((item: any) => (
+                        <div 
+                            key={item.id}
+                            style={{
+                                backgroundImage: `url(${item.image})`,
+                                width: '33.33%',
+                                height: '400px',
+                                backgroundSize: "cover",
+                                color:'white',
+                                display: 'flex',
+                                justifyContent: 'center',
+                            }}
+                        >
+                            <h2 className='text-white text-lg text-shadow-lg'>{item.name}</h2>
+                        </div>
+                    ))}
+                </div>
+            ))}
         </>
     )
 }
